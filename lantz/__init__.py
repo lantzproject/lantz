60490adf5ea186c843cf627f511970e9f3462aa6
Hernan Grecco <hernan.grecco@gmail.com>
2018-03-30 14:20:48 -0300

    Do not require qt in lantz __init__

--- lantz/__init__.py
+++ lantz/__init__.py
@@ -1,12 +1,20 @@
 
 import lantz_core as core
 import lantz_drivers as drivers
-import lantz_qt as qt
 import lantz_sims as sims
 
 from lantz_core import Driver, Feat, DictFeat, Action, MessageBasedDriver, Q_
 
-__all__ = ['core', 'drivers', 'qt', 'sims', 'log']
+__all__ = ['core', 'drivers', 'qt', 'sims', 'log',
+           'Driver', 'Feat', 'DictFeat', 'Action',
+           'MessageBasedDriver', 'Q_']
+
+try:
+    import lantz_qt as qt
+    __all__.append('qt')
+except ImportError:
+    pass
+
 
 from lantz_core import errors, log
 
