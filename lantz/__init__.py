c03f3d3f59abc0805ceb9f36d68b5c230840ecb6
Hernan <hernan.grecco@gmail.com>
2018-11-21 15:41:25 -0300

    Added imports for convenience

--- lantz/__init__.py
+++ lantz/__init__.py
@@ -2,7 +2,7 @@
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from lantz.core import Driver, Feat, DictFeat, Action, MessageBasedDriver, Q_
+from lantz.core import Driver, Feat, DictFeat, Action, MessageBasedDriver, Q_, errors, mfeats
 
 
 import argparse
