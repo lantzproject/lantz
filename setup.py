3b72af955146b0027a903896ad95852242b2ee14
Hernan <hernan.grecco@gmail.com>
2019-01-21 14:34:11 -0300

    Preparing release 0.5.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = '\n\n'.join([read('README.md'),
 __doc__ = long_description
 
 setup(name='lantzdev',
-      version='0.5.2.dev0',
+      version='0.5.2',
       license='BSD',
       description='Instrumentation framework',
       long_description=long_description,
